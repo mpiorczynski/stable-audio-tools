75c2f5d5aab534e323341b3c943b3a271a7d5c77
Mikolaj Piorczynski <m.piorczynski13@gmail.com>
2026-01-11 01:21:02 +0100

    Back to default files

--- train.py
+++ train.py
@@ -1,12 +1,21 @@
 import torch
 import json
+import os
 import pytorch_lightning as pl
+from dotenv import load_dotenv
 
-from prefigure.prefigure import get_all_args
+from prefigure.prefigure import get_all_args, push_wandb_config
 from stable_audio_tools.data.dataset import create_dataloader_from_config
 from stable_audio_tools.models import create_model_from_config
-from stable_audio_tools.models.utils import copy_state_dict, load_ckpt_state_dict
-from stable_audio_tools.training import create_training_wrapper_from_config
+from stable_audio_tools.models.utils import (
+    copy_state_dict,
+    load_ckpt_state_dict,
+    remove_weight_norm_from_model,
+)
+from stable_audio_tools.training import (
+    create_training_wrapper_from_config,
+    create_demo_callback_from_config,
+)
 
 
 class ExceptionCallback(pl.Callback):
@@ -23,12 +32,14 @@ class ModelConfigEmbedderCallback(pl.Callback):
 
 
 def main():
+    load_dotenv()
     torch.multiprocessing.set_sharing_strategy("file_system")
     args = get_all_args()
     seed = args.seed
 
     pl.seed_everything(seed, workers=True)
 
+    # Get JSON config from args.model_config
     with open(args.model_config) as f:
         model_config = json.load(f)
 
@@ -46,6 +57,7 @@ def main():
     )
 
     val_dl = None
+    val_dataset_config = None
 
     if args.val_dataset_config:
         with open(args.val_dataset_config) as f:
@@ -66,11 +78,46 @@ def main():
     if args.pretrained_ckpt_path:
         copy_state_dict(model, load_ckpt_state_dict(args.pretrained_ckpt_path))
 
+    if args.remove_pretransform_weight_norm == "pre_load":
+        remove_weight_norm_from_model(model.pretransform)
+
+    if args.pretransform_ckpt_path:
+        model.pretransform.load_state_dict(
+            load_ckpt_state_dict(args.pretransform_ckpt_path)
+        )
+
+    # Remove weight_norm from the pretransform if specified
+    if args.remove_pretransform_weight_norm == "post_load":
+        remove_weight_norm_from_model(model.pretransform)
+
     training_wrapper = create_training_wrapper_from_config(model_config, model)
 
     exc_callback = ExceptionCallback()
 
-    checkpoint_dir = args.save_dir if args.save_dir else None
+    if args.logger == "wandb":
+        logger = pl.loggers.WandbLogger(project=args.name)
+        logger.watch(training_wrapper)
+
+        if args.save_dir and isinstance(logger.experiment.id, str):
+            checkpoint_dir = os.path.join(
+                args.save_dir,
+                logger.experiment.project,
+                logger.experiment.id,
+                "checkpoints",
+            )
+        else:
+            checkpoint_dir = None
+    elif args.logger == "comet":
+        logger = pl.loggers.CometLogger(project_name=args.name)
+        if args.save_dir and isinstance(logger.version, str):
+            checkpoint_dir = os.path.join(
+                args.save_dir, logger.name, logger.version, "checkpoints"
+            )
+        else:
+            checkpoint_dir = args.save_dir if args.save_dir else None
+    else:
+        logger = None
+        checkpoint_dir = args.save_dir if args.save_dir else None
 
     ckpt_callback = pl.callbacks.ModelCheckpoint(
         every_n_epochs=1,
@@ -80,22 +127,79 @@ def main():
     )
     save_model_config_callback = ModelConfigEmbedderCallback(model_config)
 
+    if args.val_dataset_config:
+        demo_callback = create_demo_callback_from_config(model_config, demo_dl=val_dl)
+    else:
+        demo_callback = create_demo_callback_from_config(model_config, demo_dl=train_dl)
+
+    # Combine args and config dicts
+    args_dict = vars(args)
+    args_dict.update({"model_config": model_config})
+    args_dict.update({"dataset_config": dataset_config})
+    args_dict.update({"val_dataset_config": val_dataset_config})
+
+    if args.logger == "wandb":
+        push_wandb_config(logger, args_dict)
+    elif args.logger == "comet":
+        logger.log_hyperparams(args_dict)
+
+    # Set multi-GPU strategy if specified
+    if args.strategy:
+        if args.strategy == "deepspeed":
+            from pytorch_lightning.strategies import DeepSpeedStrategy
+
+            strategy = DeepSpeedStrategy(
+                stage=2,
+                contiguous_gradients=True,
+                overlap_comm=True,
+                reduce_scatter=True,
+                reduce_bucket_size=5e8,
+                allgather_bucket_size=5e8,
+                load_full_weights=True,
+            )
+        else:
+            strategy = args.strategy
+    else:
+        strategy = "ddp_find_unused_parameters_true" if args.num_gpus > 1 else "auto"
+
+    val_args = {}
+
+    if args.val_every > 0:
+        val_args.update(
+            {
+                "check_val_every_n_epoch": None,
+                "val_check_interval": args.val_every,
+            }
+        )
+
     trainer = pl.Trainer(
         devices=args.num_gpus,
         accelerator="gpu",
-        strategy="auto",
+        strategy=strategy,
         precision=args.precision,
-        callbacks=[ckpt_callback, exc_callback, save_model_config_callback],
-        logger=None,
+        accumulate_grad_batches=args.accum_batches,
+        callbacks=[
+            ckpt_callback,
+            demo_callback,
+            exc_callback,
+            save_model_config_callback,
+        ],
+        logger=logger,
         log_every_n_steps=1,
         max_epochs=args.max_epochs,
         default_root_dir=args.save_dir,
         gradient_clip_val=args.gradient_clip_val,
         reload_dataloaders_every_n_epochs=0,
         num_sanity_val_steps=0,  # If you need to debug validation, change this line
+        **val_args,
     )
 
-    trainer.fit(training_wrapper, train_dl, val_dl)
+    trainer.fit(
+        training_wrapper,
+        train_dl,
+        val_dl,
+        ckpt_path=args.ckpt_path if args.ckpt_path else None,
+    )
 
 
 if __name__ == "__main__":
